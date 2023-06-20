cd8b2c2ec8b374ec7c2d1f6f0e475f99e4838985
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-06-20 15:01:34 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, searchString){
+    return drivers.filter(function(drivers){
+    return drivers.toLowerCase() === searchString.toLowerCase();
+    })
+}   
+
+const drivers1 = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function fuzzyMatch(drivers, query){
+    return drivers.filter(function(driver){
+        return driver.toLowerCase().startsWith(query.toLowerCase())
+    })
+}
+function matchName(drivers, searchString) {
+    return drivers.filter(function(driver) {
+      return driver.name.toLowerCase() === searchString.toLowerCase();
+    });
+  }
